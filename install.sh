c8e81a0c1246d464a17e2f6caae614070989bdb5
UnaTried <103455203+UnaTried@users.noreply.github.com>
2025-03-05 17:13:29 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ case "$1" in
 *)
     echo "----------------------------------------"
     read -p "Are you using Arch Linux or not? [Y/n] " arch
-    if $arch = "N" || $arch = "n"; then
+    if $arch == "N" || $arch == "n"; then
         arch
     else
         other

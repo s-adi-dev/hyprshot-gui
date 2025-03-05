67283470daea07ae40ac09f313abfd5286994fa1
UnaTried <103455203+UnaTried@users.noreply.github.com>
2025-03-05 18:42:59 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ case "$1" in
 *)
     echo "----------------------------------------"
     while true; do
-        read -r -p "Are you using Arch Linux or not? [y/n] " yes
+        read -r -p "Are you using Arch Linux or not? [y/n] " yes < /dev/tty
         case "$yes" in
         [Nn]) 
             other
